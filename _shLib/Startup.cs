6d4f6fb665379655b683214bc5f9a5e6ff9f13ef
shinsetsu <shinsetsu@protonmail.com>
2020-07-03 13:27:14 -0500

    ___________
    -->  CHK  -  ->
    _______________________________________________

--- _shLib/Startup.cs
+++ _shLib/Startup.cs
@@ -1,13 +1,15 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Westwind.AspNetCore.LiveReload;
 
 namespace _shLib
 {
@@ -23,15 +25,54 @@ namespace _shLib
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddRazorPages();
-		}
+			services.AddLiveReload(config =>
+			{
+				// optional - use config instead
+				//config.LiveReloadEnabled = true;
+				//config.FolderToMonitor = Path.GetFullname(Path.Combine(Env.ContentRootPath,"..")) ;
+			});
 
+
+
+
+
+
+
+			//WebHost = Configuration["WebRoot"];
+			//if (string.IsNullOrEmpty(WebRoot))
+			//	WebRoot = Environment.CurrentDirectory;
+
+			//var razEnabled = Configuration["RazorEnabled"];
+			//UseRazor = string.IsNullOrEmpty(razEnabled) ||
+			//	     !razEnabled.Equals("false", StringComparison.InvariantCultureIgnoreCase);
+
+
+			// alt+254 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+			// alt+254 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+				//#if USE_RAZORPAGES
+				//if (UseRazor)
+				//{
+				//services.AddRazorPages(opt => { opt.RootDirectory = "/"; })
+				//.AddRazorRuntimeCompilation(
+				//opt =>
+				//{
+				//// This would be useful but it's READ-ONLY
+				//// opt.AdditionalReferencePaths = Path.Combine(WebRoot,"bin");
+				//opt.FileProviders.Add(new PhysicalFileProvider(WebRoot));
+				//});
+				//}
+				//#endif
+			// alt+254 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+			// alt+254 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+		}
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
 			{
-				app.UseDeveloperExceptionPage();
+				//app.UseDeveloperExceptionPage();
+				//app.UseDatabaseErrorPage();
+				app.UseLiveReload();  //Install-Package WestWind.AspnetCore.LiveReload      ---  This only Require Dotnet.watch.run
 			}
 			else
 			{
