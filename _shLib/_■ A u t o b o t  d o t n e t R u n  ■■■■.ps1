7850acd8033903bf77fda2c3056199942e6da67a
shinsetsu <shinsetsu@protonmail.com>
2020-07-03 13:45:50 -0500

    ~

--- _shLib/_■ A u t o b o t  d o t n e t R u n  ■■■■.ps1
+++ _shLib/_■ A u t o b o t  d o t n e t R u n  ■■■■.ps1
@@ -1,4 +1,7 @@
-﻿  Start-Process "chrome.exe" "	https://localhost:1999	"
+﻿#Install Node & NPM For this to work...   https://nodejs.org/en/download/
+
+
+  Start-Process "chrome.exe" "	https://localhost:1999	"
 
 
   #AWS server access in a tab...
@@ -8,19 +11,20 @@
 		#start "C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe"
 		#		 $serverName -E          possible flag for opening and feeding config....
 
+  dotnet watch run
 
-  #####start-process "chrome.exe" "",'--profile-directory="Default"'
-dotnet watch run
 
+####        #Set Powershell to .......
 #set-ExecutionPolicy unrestricted
 #https://docs.west-wind.com/westwind.utilities/index.htm
 
 #https://weblog.west-wind.com/posts/2019/May/18/Live-Reloading-Server-Side-ASPNET-Core-Apps#proxying-aspnet-core-for-code-injection
-#dotnet watch run
 
-#Install Node & NPM For this to work...
 
-#npm install -g browser-sync
+##		These are a settings added into     apsettings.json.....    
+#		click drop down arrow,..   appsettings.Development.json
+#			Which file types to triggerr reloads,...  
+##
 
 #browser-sync start `
  #   --proxy http://localhost:5001/ `
